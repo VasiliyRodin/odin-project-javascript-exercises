28a555a46f82901049647206ea963d3c535171bc
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-08-09 11:36:53 -0700

    complete 06_repeat_string

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let text = "";
+    if (num<0){
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        //concat the strings n number of times
+        text = text.concat(string);
+    }
+    return text;
 };
 
 // Do not edit below this line
