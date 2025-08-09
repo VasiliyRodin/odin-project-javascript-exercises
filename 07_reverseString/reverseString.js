31a1e2b716c2e989f37336603ca52c810041e375
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-08-09 11:45:37 -0700

    complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split("");
+    const reverseSpring = splitString.reverse();
+    return reverseSpring.join("");
 };
 
 // Do not edit below this line
